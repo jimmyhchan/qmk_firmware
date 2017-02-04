98b4e3e893dccf764a77026fba1455ebc0b63186
Jimmy Chan <jchan@linkedin.com>
2017-02-03 21:40:02 -0800

    add eeprom...yet it doesnt work

---
+++ keyboards/lets_split/flash_eeprom_right.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+# disconnect the TRRS. Plug the USB into the right hand
+# then run this script
+
+# Set EE_HANDS in your config.h and remove any I2C_MASTER_*
+# build another hex file and flash it to both sides
+
+# CHANGE ME make sure you figure out what port your device is on
+# PORT=/dev/tty.usbmodem1421
+PORT=/dev/tty.usbmodem1411
+
+avrdude -v -patmega32u4 -cavr109 -P$PORT -Ueeprom:w:eeprom-righthand.eep
--- keyboards/lets_split/keymaps/jimmy/config.h
+++ keyboards/lets_split/keymaps/jimmy/config.h
@@ -15,16 +15,22 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// if you soldered the i2c jumpers and the resistors, you can use either
-// if you have not, you must use serial
-//#define USE_I2C
-#define USE_SERIAL
 
-//  use EE_HANDS and flash to eeprom if you want to regularly swap the usb side
-//  otherwise choose what side you want the usb cable to plug into
+// HEY jimmy! cd to the lets_split directory and run make rev2-jimmy
+
+
+// if you soldered the i2c jumpers and the resistors, you can use either serial or i2c
+// if you have not, you must use serial
+#define USE_I2C
+//#define USE_SERIAL
+
+//  otherwise choose what side you want the usb cable to plug into (defaults to LEFT)
 //#define I2C_MASTER_LEFT
 //#define I2C_MASTER_RIGHT
-//#define EE_HANDS
+
+// can't get EE prom working :(
+// use EE_HANDS if you want to regularly swap the usb side and you have flashed the eeprom to each side
+// #define EE_HANDS
 
 #ifdef SUBPROJECT_rev1
     #include "../../rev1/config.h"

ab38afb65208171d1d24185207a8ed947d5ef747
Jimmy Chan <jchan@linkedin.com>
2017-03-05 07:13:35 -0800

    derp too many rows for the 3rd hand

--- keyboards/lets_split/keymaps/rightCol/keymap.c
+++ keyboards/lets_split/keymaps/rightCol/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                                                                           |    6   |
  *                                                                                           `--------'
  */
-[_DVORAK] = KEYMAP_EXTRA_RIGHT_COLUMN(\ 
+[_DVORAK] = KEYMAP_EXTRA_RIGHT_COLUMN( \
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,           KC_1,\
   KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,           KC_2,\
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT ,           KC_3,\
